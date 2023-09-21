a3014c5a4ab1e55e8ab66c278aaf057146f8b8f2
BryanCruz94 <bscruz@espe.edu.ec>
2023-09-21 14:58:59 -0500

    Imagen 1 y 2

--- src/app/estadistica-u/grafico2-f2/grafico2-f2.component.ts
+++ src/app/estadistica-u/grafico2-f2/grafico2-f2.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { ConnectService } from 'src/app/service/connect.service';
 import { Transaction } from '../../interfaces/incidentes.interface';
-import { Chart, ChartConfiguration, ChartData, ChartTypeRegistry, ChartDataset } from 'chart.js';
+import { Chart, ChartData } from 'chart.js';
 import { Observable } from 'rxjs';
- 
+
 @Component({
-  selector: 'app-grafico2',
-  templateUrl: './grafico2.component.html',
-  styleUrls: ['./grafico2.component.css']
+  selector: 'app-grafico2-f2',
+  templateUrl: './grafico2-f2.component.html',
+  styleUrls: ['./grafico2-f2.component.css']
 })
-export class Grafico2Component implements OnInit {
+export class Grafico2F2Component implements OnInit {
   public chart!: Chart;
 
   constructor(private connectService: ConnectService) { }
@@ -17,7 +17,7 @@ export class Grafico2Component implements OnInit {
   public filteredTransactions!: Observable<Transaction[]>;
 
   ngOnInit(): void {
-    this.filteredTransactions = this.connectService.filteredTransactions;
+    this.filteredTransactions = this.connectService.filteredTransactionsUE;
     this.filteredTransactions.subscribe((transactions: Transaction[]) => {
       this.createBarChart(transactions);
     });
@@ -28,7 +28,7 @@ export class Grafico2Component implements OnInit {
     const data: ChartData<any, any> = {
       labels: labels,
       datasets: [{
-        label: 'Número de incidentes',
+        label: 'Número de incidentes según filtro',
         data: transactions.map(transaction => transaction.cost as number),
         backgroundColor: 'rgba(75, 192, 40, 0.6)',
         borderColor: 'rgba(75, 192, 40, 1)',
